a5a52ff6688d3f90951ea28945dd74c39a008c19
duytungqua <quangduy.hcmut@gmail.com>
2025-06-13 16:41:00 +0700

    tut: impl data type

--- core_go/01_basics/data_types.go
+++ core_go/01_basics/data_types.go
@@ -8,5 +8,16 @@ func dataTypes() {
 	var name string = "D"
 	fmt.Println("Name:", name)
 	fmt.Println("Age:", age)
-	
+
+	var scores = []int{10, 20, 30, 40, 50}
+	fmt.Println("Scores:", scores)
+	var isActive bool = true // boolean type
+	fmt.Println("Is Active:", isActive)
+
+	person := struct {
+		Name string
+		Age  int
+	}{"D", 30}
+	fmt.Println("Person:", person)
+	fmt.Println("Person Name:", person.Name)
 }
