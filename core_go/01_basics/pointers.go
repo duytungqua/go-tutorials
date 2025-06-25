ef330cecd9fe5d0f04bff138f09868c8c867c14b
duytungqua <quangduy.hcmut@gmail.com>
2025-06-25 12:28:50 +0700

    tut: update pointers in golang

--- core_go/01_basics/pointers.go
+++ core_go/01_basics/pointers.go
@@ -12,7 +12,19 @@ func incrementValue(value int) {
 	Pointers are variables that store the memory address of another variable.
 	Declaring a pointer use the * operator before the type.
 	var ptr *int
-	To get value of a pointer, we use the & operator to get the address of a variable.
+	To get value of a pointer , we use the & operator to get the address of a variable.
 	var value int = 10
 	We can not assign a value to a pointer directly, we need to use the address of the variable.
 */
+
+func pointers() {
+	var ptr *int // Declare a pointer to an int
+	var value int = 10
+	ptr = &value
+
+	fmt.Printf("value", value)
+	fmt.Printf("Pointer address: %p\n", ptr) // Print the address stored in the
+	fmt.Printf("Pointer value: %d\n", *ptr) // Dereference the pointer to get the value
+
+
+}
