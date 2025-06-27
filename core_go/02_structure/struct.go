0d3fddf3fb78d36fa01992f2aa51cc93ed2c529a
duytungqua <quangduy.hcmut@gmail.com>
2025-06-27 17:56:20 +0700

    tut: continue struct person

--- core_go/02_structure/struct.go
+++ core_go/02_structure/struct.go
@@ -13,3 +13,12 @@ func test() {
 		Age: 30,
 	}	
 }
+
+type Person struct {
+	name string
+	age  int
+}	
+
+func (p Person) String() string {
+	return fmt.Sprintf("Name: %s, Age: %d", p.name, p.age)
+}
