0d367e6a004789aca126b1ddc00f1ff3f70cd1ae
duytungqua <quangduy.hcmut@gmail.com>
2025-06-29 23:58:22 +0700

    tut: update method

--- core_go/02_structure/method.go
+++ core_go/02_structure/method.go
@@ -10,3 +10,21 @@ type Person struct {
 func (p Person) String() string { // Value receiver
 	return fmt.Sprintf("Name: %s, Age: %d", p.Name, p.age)
 }	
+// NewPerson creates a new Person instance
+func NewPerson(name string, age int) Person {
+	return Person{
+		Name: name,
+		age:  age,
+	}
+}	
+//create instance of Person
+func CreatePerson() {
+	person := Person{
+		Name: "Alice",
+		age:  30,
+	}
+
+	fmt.Println(person.String()) // Accessing the String method
+	person.age = 35              // Modifying the age field
+	fmt.Println(person.String()) // Accessing the String method again	
+}
--- core_go/02_structure/struct.go
+++ core_go/02_structure/struct.go
@@ -6,18 +6,8 @@ func test() {
 		Name string
 		Age  int
 	}
-
-	// Create an instance of Person
-	person := Person{
-		Name: "Alice",
-		Age: 30,
-	}	
+	
 }
-
-type Person struct {
-	name string
-	age  int
-}	
 
 func (p Person) String() string {
 	return fmt.Sprintf("Name: %s, Age: %d", p.name, p.age)
