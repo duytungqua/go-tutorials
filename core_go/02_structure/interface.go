decfc53340ce025883196084440ee02a59958184
duytungqua <quangduy.hcmut@gmail.com>
2025-07-11 16:48:17 +0700

    feat: add new interface

--- core_go/02_structure/interface.go
+++ core_go/02_structure/interface.go
@@ -1,26 +1,14 @@
 package structure
 
-func init() {
-	type Person struct {
-		Name string
-		Age  int
-	}
+/* 
+interface definning set of methods signatures, interface is a powerful feature to achive polymorphism
+Key featueres of interfaces: 
+1. Methods: Interfaces define a set of method that a type must implement
+2. Dynamic Typing: Interfaces allow for dynamic typing, meaning that a variable of an interface can hold values 
+3. Empty Interface: The empty interface `interface{}` can hold values of any type  var anyValue interface{}
+4. Type Assertion: You can use type assertion to retrieve the underlying value
+*/
 
-	// Create an instance of Person
-	person := Person{
-		Name: "Alice",
-		Age:  30,
-	}
-}	
-
-interface PersonInterface {
-	String() string
-}
-
-// NewPerson creates a new Person instance
-func NewPerson(name string, age int) Person {
-	return Person{
-		Name: name,
-		Age:  age,
-	}
+type Greeter interface{
+	greeter();
 }
