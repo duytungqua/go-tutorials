20f7e4da26c3b79f929ee6106b2f9fbaa2b53463
duytungqua <quangduy.hcmut@gmail.com>
2025-07-13 23:19:10 +0700

    tut: update receiver value

--- core_go/02_structure/method.go
+++ core_go/02_structure/method.go
@@ -28,3 +28,20 @@ func CreatePerson() {
 	person.age = 35              // Modifying the age field
 	fmt.Println(person.String()) // Accessing the String method again	
 }
+
+// example of exported and unexported fields
+type PersonWithFields struct {
+	Name string // Exported field (accessible outside the package)
+	age  int    // Unexported field (not accessible outside the package)
+}
+
+func (p PersonWithFields) GetAge() int {
+	return p.age // Accessing the unexported field through a method
+}
+//go constructors
+func NewPersonWithFields(name string, age int) PersonWithFields {
+	return PersonWithFields{
+		Name: name,
+		age:  age,
+	}
+}
