65e2a85aebe72440590471b7887e6a242b144b6d
duytungqua <quangduy.hcmut@gmail.com>
2025-07-14 22:20:38 +0700

    tut: updater struct

--- core_go/02_structure/struct.go
+++ core_go/02_structure/struct.go
@@ -1,58 +1,23 @@
 package structure
+
 import "fmt"
 
-func test() {
-	type Person struct {
-		Name string
-		Age  int
-	}
-	
+// Person struct with embedded Address struct
+type Persons struct {
+	Name    string
+	Age     int
+	Address // Embedded struct
 }
 
-func (p Person) String() string {
-	return fmt.Sprintf("Name: %s, Age: %d", p.name, p.age)
-}
 
-// NewPerson creates a new Person instance
-func NewPerson(name string, age int) Person {
-	return Person{
-		name: name,
-		age:  age,
-	}
-}
-
-//create instance of Person
-func CreatePerson() {
-	person := Person{
-		name: "Alice",
-		age:  30,
-	}
-
-	fmt.Println(person.String()) // Accessing the String method
-	person.age = 35              // Modifying the age field
-	fmt.Println(person.String()) // Accessing the String method again	
-}
-
-//exported and unexported fields
-type PersonWithFields struct {
-	Name string // Exported field (accessible outside the package)
-	age  int    // Unexported field (not accessible outside the package)
-}
-func (p PersonWithFields) GetAge() int {
-	return p.age // Accessing the unexported field through a method
-}	
-
-//go constructors
-func NewPersonWithFields(name string, age int) PersonWithFields {
-	return PersonWithFields{
+// NewPerson creates a new Person instance with embedded Address
+func NewPerson(name string, age int, city string, state string) Person {
+	return Persons{
 		Name: name,
-		age:  age,
+		Age:  age,
+		Address: Address{
+			City:  city,
+			State: state,
+		},
 	}
-}
-//methods with value and pointer receivers
-func (p PersonWithFields) ValueReceiverMethod() {
-	fmt.Println("Value receiver method called")
-}	
-func (p *PersonWithFields) PointerReceiverMethod() {
-	fmt.Println("Pointer receiver method called")
 }
