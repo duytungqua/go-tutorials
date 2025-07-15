fbe39cb7d8e5b92f8d6da6448b25a4d13ec402f8
duytungqua <quangduy.hcmut@gmail.com>
2025-07-15 23:10:37 +0700

    tut: update struct

--- core_go/02_structure/struct.go
+++ core_go/02_structure/struct.go
@@ -9,4 +9,21 @@ type Persons struct {
 	Address // Embedded struct
 }
 
+// Address struct
+type Addres struct {
+	City  string
+	State string
+}
+
+// NewPerson creates a new Person instance with embedded Address
+func NewPersons(name string, age int, city string, state string) Persons {
+	return Persons{
+		Name: name,
+		Age:  age,
+		Address: Addres{
+			City:  city,
+			State: state,
+		},
+	}
+}
 
