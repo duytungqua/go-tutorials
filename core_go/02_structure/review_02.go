4610e2ca058ded315c331ac4b611392f8b8bf07a
duytungqua <quangduy.hcmut@gmail.com>
2025-07-21 17:34:57 +0700

    tut: create value receiver

--- core_go/02_structure/review_02.go
+++ core_go/02_structure/review_02.go
@@ -15,3 +15,18 @@ type Department struct {
 	Name string
 	Employee // Embedded struct
 }
+
+//value receiver method 
+/*
+	func (v T) method(){
+	}
+
+	1. Encapsulates we dont need to pass the struct as a parameter when calling the method
+	2. Can access the fields of the struct directly
+	3. Immutable, cannot modify the originnal struct
+*/
+
+func (d Department) GetEmployeeName() string {
+	return d.Employee.Name
+}
+
