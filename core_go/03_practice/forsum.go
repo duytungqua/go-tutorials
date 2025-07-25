b56585d9c0065737de0ebf3a38884c009dd60ce4
duytungqua <quangduy.hcmut@gmail.com>
2025-07-25 23:03:42 +0700

    tut: update practice

--- core_go/03_practice/forsum.go
+++ core_go/03_practice/forsum.go
@@ -3,5 +3,17 @@ package practice
 import "fmt"	
 
 func Sum() {
+	var sum int = 0
+	for i := 1; i <= 10; i++ {
+		sum += i
+	}
+	fmt.Println("Sum of first 10 natural numbers is:", sum)
 	
+	var numbers = []int{1, 2, 3, 4, 5}
+	var total int = 0
+	for _, num := range numbers {
+		total += num
+	}
+	fmt.Println("Sum of the slice is:", total)
 }
+s
