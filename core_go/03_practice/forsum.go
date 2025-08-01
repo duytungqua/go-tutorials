1ec937e4bcc0f255af2490f473aca2da03f380a1
duytungqua <quangduy.hcmut@gmail.com>
2025-08-01 23:17:04 +0700

    tut: update for sum

--- core_go/03_practice/forsum.go
+++ core_go/03_practice/forsum.go
@@ -2,8 +2,22 @@ package practice
 
 import "fmt"	
 
-func Sum() {
+func F4Sum() {
 	
-	
+	//use two pointers to iterate through the array
+	arr := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	target := 10
+	for i, j := 0, len(arr)-1; i < j; {
+		sum := arr[i] + arr[j]
+		if sum == target {
+			fmt.Printf("Found a pair: %d + %d = %d\n", arr[i], arr[j], target)
+			i++
+			j--
+		} else if sum < target {
+			i++
+		} else {
+			j--
+		}
+	}
 
 }
